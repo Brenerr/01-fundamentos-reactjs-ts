271c06ac96085756af8782326ef301917b885dcf
Brener Alves <brener820@gmail.com>
2022-08-29 23:39:03 -0300

    Fundamentos do ReactJS

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,27 @@
+import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+            />
+
+            <div className={styles.profile}>
+                <Avatar src="https://github.com/brenerr.png"/>
+                <strong>Brener Alves</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    );
+}
