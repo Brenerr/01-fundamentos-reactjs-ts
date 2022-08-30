f5e53fe11e8f14e4bb1bf7fb722e70ce2546fa2f
Brener Alves <brener820@gmail.com>
2022-08-30 12:58:42 -0300

    fix interface Post

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Header } from './components/Header';
-import { Post } from './components/Post';
+import { Post, PostProps } from './components/Post';
 import { Sidebar } from './components/Sidebar';
 
 import styles from './App.module.css';
 import './global.css';
 
+interface Post extends PostProps {
+  id: number;
+}
 
-const posts = [
+
+const posts: Post[] = [
   {
     id: 1,
     author: {
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -18,7 +18,7 @@ interface Content {
     content: string;
 }
 
-interface PostProps {
+export interface PostProps {
     author: Author;
     publishedAt: Date;
     content: Content[];
